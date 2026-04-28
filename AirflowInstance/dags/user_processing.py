7a6c53e2faab82b14254f274ef44d86df8768f3a
K-ES <your@email>
2026-04-28 16:21:19 +0300

    ухожу в контекстные менеджеры

--- AirflowInstance/dags/user_processing.py
+++ AirflowInstance/dags/user_processing.py
@@ -1,33 +1,12 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.operators.python import PythonOperator
-from pandas import json_normalize 
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-import json
-
-def _process_user(ti):
-    user = ti.xcom_pull(task_ids="extract_user")
-    user = user['results'][0]
-    processed_user = json_normalize({'firstname': user['name']['first'], 
-                                     'lastname' : user['name']['last'], 
-                                     'country'  : user['location']['country'],
-                                     'username' : user['login']['username'],
-                                     'password' : user['login']['password'], 
-                                     'email'    : user['email']})
-    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
-    
-def _store_user():
-    hook = PostgresHook(postgres_conn_id='postgres',)
-    hook.copy_expert(sql = "COPY users FROM stdin WITH DELIMITER as ',' ", filename='/tmp/processed_user.csv')
 
 with DAG(
     'user_processing',
     start_date=datetime(2023, 1, 1),
-    schedule_interval='@daily',        # было schedule_interval
-    catchup=False             # было catchup
+    schedule_interval='@daily',
+    catchup=False
 ) as dag:
 
     create_table = PostgresOperator(
@@ -43,12 +22,3 @@ with DAG(
         );
         '''
     )
-    
-    is_api_available = HttpSensor(task_id='is_api_available', http_conn_id='user_api', endpoint='api/')
-    
-    extract_user = SimpleHttpOperator(task_id='extract_user', http_conn_id='user_api', endpoint='api/', method='GET',
-                                      response_filter=lambda response: json.loads(response.text))
-                                      
-    process_user = PythonOperator(task_id='process_user', python_callable=_process_user,)
-    
-    store_user = PythonOperator(task_id='store_user', python_callable=_store_user)
